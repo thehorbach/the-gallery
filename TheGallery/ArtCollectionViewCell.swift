fed8de53d0155746cb4057c4a5ace5fd7e761d5b
Slavik <vjacheslav.horbach@gmail.com>
2016-10-14 12:27:36 +0100

    finished collection View

---
+++ TheGallery/ArtCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ArtCollectionViewCell.swift
+//  TheGallery
+//
+//  Created by Vyacheslav Horbach on 14/10/16.
+//  Copyright © 2016 Homework Tutor LTD. All rights reserved.
+//
+
+import UIKit
+
+class ArtCollectionViewCell: UICollectionViewCell {
+    
+}
--- TheGallery/ViewController.swift
+++ TheGallery/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +25,23 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.collectionView.bounds.size.width - 80, height: self.collectionView.bounds.size.height - 40)
+    }
 }
 
