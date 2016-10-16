5953dd064246d37982e793eb15f4fb541bcf343f
Slavik <vjacheslav.horbach@gmail.com>
2016-10-17 00:14:40 +0100

    added coredata

---
+++ Art+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Art+CoreDataClass.swift
+//  TheGallery
+//
+//  Created by Vyacheslav Horbach on 16/10/16.
+//  Copyright © 2016 Homework Tutor LTD. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Art: NSManagedObject {
+
+}
--- TheGallery/ViewController.swift
+++ TheGallery/ViewController.swift
@@ -7,30 +7,77 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var gallery = [Art]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
+        
+        updateGallery()
+        
+        if gallery.count == 0 {
+            createArt(title: "hello", productIdentifier: "", imageName: "", purchased: true)
+            createArt(title: "hello", productIdentifier: "", imageName: "", purchased: true)
+            createArt(title: "hello", productIdentifier: "", imageName: "", purchased: true)
+            
+            updateGallery()
+            self.collectionView.reloadData()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func createArt(title: String, productIdentifier: String, imageName: String, purchased: Bool) {
+        let context = getContext()
+        
+        if let entity = NSEntityDescription.entity(forEntityName: "Art", in: context) {
+            
+            let art = NSManagedObject(entity: entity, insertInto: context)
+            
+            art.setValue(title, forKey: "title")
+            art.setValue(productIdentifier, forKey: "productIdentifier")
+            art.setValue(imageName, forKey: "imageName")
+            art.setValue(purchased.hashValue, forKey: "purchased")
+            
+            do {
+                try context.save()
+            } catch {
+                print(error)
+            }
+        }
     }
-
-
+    
+    
+    func getContext () -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    func updateGallery() {
+        let context = getContext()
+        
+        let fetchRequest: NSFetchRequest<Art> = Art.fetchRequest()
+        
+        do {
+            let searchResults = try context.fetch(fetchRequest)
+            self.gallery = searchResults
+        } catch {
+            print(error)
+        }
+        
+    }
 }
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return gallery.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
